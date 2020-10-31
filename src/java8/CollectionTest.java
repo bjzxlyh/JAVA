da705cae79730aae93baadcafca30e7747944063
liyiheng <834805011@qq.com>
2020-10-31 19:55:15 +0800

    CollectionTest

---
+++ src/java8/CollectionTest.java
@@ -0,0 +1,33 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        CollectionTest co = new CollectionTest();
+        co.test1();
+    }
+         public  void test1(){
+             Collection coll = new ArrayList();
+
+             coll.add("A");
+             coll.add("B");
+             coll.add(123);
+             coll.add(new Date());
+             System.out.println(coll.size());
+
+             Collection coll1 = new ArrayList();
+             coll1.add(234);
+             coll1.add("SS");
+             coll.addAll(coll1);
+             System.out.println(coll.size());
+             System.out.println(coll);
+
+             System.out.println(coll.isEmpty());
+             coll.clear();
+             System.out.println(coll.isEmpty());
+
+         }
+}
